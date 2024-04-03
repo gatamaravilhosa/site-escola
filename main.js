9424a28f8264e9688644ce59bef2019fec406a48
gatamaravilhosa <132580102+gatamaravilhosa@users.noreply.github.com>
2024-04-03 12:47:53 -0300

    03/04

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = document. querrySeletorALL(".botao");
+const textos  = document. querrySeletorALL(".aba-conteudo");
+for(let i=0; i<botoes.leght; i++){      
+ botoes[i].onelink = function ()
+ for(let j=0;j<botoes.lenght; j++){
+    botoes[j].classList.remove("ativo");   
+    textos[j].classList.remove("ativo");   {
+    botoes[j].classList.add("ativo");
+    botoes[j].classList.add("ativo");
+    }
